e8c2993263a76d096e25f29133309fb30e4aac70
Ahmet Elgun <ahmetelgn@gmail.com>
2021-06-16 21:15:58 +0300

    first commit

---
+++ models.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+Base = declarative_base()
+engine = create_engine(DATABASE_URL)
+Session = sessionmaker(bind=engine)
+
+class User(Base):
+    __tablename__ = 'Users'
+
+    id = Column(Integer, primary_key=True)
+    firstname = Column(String(32), nullable=False)
+    lastname = Column(String(32), nullable=False)
+    email = Column(String(64), nullable=False, unique=True)
+    password = Column(String(128), nullable=False)
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
