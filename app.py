5a3f785ef51030d0dd7c553f074af829249ad783
Ahmet Elgun <ahmetelgn@gmail.com>
2021-06-28 21:01:56 +0300

    create login and register endpoints

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
-from flask import Flask, g, request
+from flask import Flask, g, request, make_response
 
 from service import set_token_to_response, refresh_jwt
+from controllers.auth import register, login
 
 app = Flask(__name__)
 
 
 @app.before_request
 def before_request():
-    try:
-        token = request.cookies.get(token)
-    except:
-        token = ""
-    g.token = token
+    g.token = request.cookies.get("token")
 
 
 @app.after_request
@@ -20,6 +17,16 @@ def after_request(resp):
         new_token = refresh_jwt(g.token)
         return set_token_to_response(resp, new_token)
     return set_token_to_response(resp)
+
+
+@app.route('/register', methods=['POST'])
+def register_func():
+    return register()
+
+
+@app.route('/login', methods=['POST'])
+def login_func():
+    return login()
 
 
 @app.route('/')
