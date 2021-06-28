0eaa40c6eec941fe4b2bb835afb2540c303dfc73
Ahmet Elgun <ahmetelgn@gmail.com>
2021-06-28 20:36:10 +0300

    fix cookie max_age

--- service/set_token_to_response.py
+++ service/set_token_to_response.py
@@ -3,10 +3,10 @@ from settings import BACKEND_DOMAIN, TOKEN_MAX_AGE
 
 def set_token_to_response(response, token=None):
     if not token:
-        response.set_cookie(
-            'token', "", max_age=1, domain=BACKEND_DOMAIN, path="/", secure=True, httponly=True)
+        response.set_cookie("token", "", max_age=TOKEN_MAX_AGE, path="/",
+                            domain=BACKEND_DOMAIN, secure=True, httponly=True, samesite="Lax")
         return response
 
-    response.set_cookie('token', token, max_age=TOKEN_MAX_AGE,
-                        domain=BACKEND_DOMAIN, path="/", secure=True, httponly=True)
+    response.set_cookie("token", token, max_age=TOKEN_MAX_AGE, path="/",
+                        domain=BACKEND_DOMAIN, secure=True, httponly=True, samesite="Lax")
     return response
